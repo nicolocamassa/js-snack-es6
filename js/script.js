7f0eb17661bcd79c8c7c811816cbc5a5aec71e98
nicolocamassa <camassanicolo03@gmail.com>
2023-12-06 15:35:04 +0100

    [UPDATE] created objects for every bike

---
+++ js/script.js
@@ -0,0 +1,30 @@
+/* SNACK 1
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare in console la bici con peso minore utilizzando destructuring e template literal */
+
+/* Array di oggetti 
+        Nome:
+        Peso:
+*/
+const bikes = [
+    {
+        nome: 'Trek',
+        peso:   10,
+    },
+    {
+        nome: 'Specialized',
+        peso:   7,
+    },
+    {
+        nome: 'Giant',
+        peso:   9,
+    },
+    {
+        nome: 'Cannondale',
+        peso:   12,
+    }
+]
+
+/* Stampa in console bici con peso minore  */
+
