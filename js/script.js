13c139b56b44b0638b70c6194918e3dfd731c465
nicolocamassa <camassanicolo03@gmail.com>
2023-12-06 17:51:53 +0100

    [UPDATE] added article list

--- js/script.js
+++ js/script.js
@@ -94,3 +94,40 @@ teams.forEach((elem) => {
 console.log(statics)
 
 
+/* BONUS 1: SNACK 3
+Abbiamo questa lista di articoli di moda:
+```
+name        type      color
+Poppy       tshirt    red
+Jumping     occhiali  blue
+CrissCross  scarpe    black
+Jenny       borsa     pink
+```
+Questi articoli sono inseriti in un array di oggetti.
+Dobbiamo attaccare a ognuno un cartellino position con una lettera dell'alfabeto generata casualmente e inserire gli oggetti così modificati in un nuovo array di oggetti. */
+
+/* Lista degli articoli */
+let articleList = [
+    {
+        name: 'Poppy',
+        type: 'tshirt',
+        color: 'red'
+    },
+    {
+        name: 'Jumping',
+        type: 'occhiali',
+        color: 'blue'
+    },
+    {
+        name: 'CrissCross',
+        type: 'scarpe',
+        color: 'black'
+    },
+    {
+        name: 'Jenny',
+        type: 'borsa',
+        color: 'pink'
+    },
+]
+
+
