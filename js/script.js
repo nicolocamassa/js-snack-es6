15baf9e5ee5397eef640f88fad8e447e9948fbcf
nicolocamassa <camassanicolo03@gmail.com>
2023-12-06 16:19:32 +0100

    [SNACK 2 COMPLETATO] add random gen & result print

--- js/script.js
+++ js/script.js
@@ -41,3 +41,56 @@ bikes.forEach((elem) => {
 
 /* Stampa in console del risultato */
 console.log(`Il peso minimo è: ${minWeight} della ${bikeName}`);
+
+
+/* SNACK 2
+Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà: punti fatti e falli subiti.
+Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+ */
+
+const teams = [
+    {
+        squadra: 'Lecce',
+        punti: 0,
+        falli: 0
+    },
+    {
+        squadra: 'Inter',
+        punti: 0,
+        falli: 0
+    },
+    {
+        squadra: 'Juve',
+        punti: 0,
+        falli: 0
+    },
+    {
+        squadra: 'Milan',
+        punti: 0,
+        falli: 0
+    }
+]
+
+/* Inizializzazione del vettore */
+let statics = []
+
+/* Ciclo per la classe teams  */
+teams.forEach((elem) => {
+    /* Destructuring */
+    let { squadra } = elem;
+
+    /* Generazione valori casuali */
+    punti = Math.floor(Math.random() * 5 );
+    falli = Math.floor(Math.random() * 5 );
+
+    /* Push usando il destructuring della squadra e dei falli */
+    statics.push({squadra, falli})
+});
+
+/* Stampa in console delle statistiche */
+console.log(statics)
+
+
